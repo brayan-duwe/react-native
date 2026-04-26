fd6f3adb52b2d51a8b6bc8d9de8971528f627fa7
Brayan Duwe <91811797+brayan-duwe@users.noreply.github.com>
2026-04-26 16:31:08 -0300

    changing background color

---
+++ src/app/_layout.tsx
@@ -0,0 +1,15 @@
+import { colors } from "@/styles/colors";
+import { Stack } from "expo-router";
+
+export default function layout() {
+  const backgroundColor = colors.gray[950];
+
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+        contentStyle: { backgroundColor },
+      }}
+    />
+  );
+}
