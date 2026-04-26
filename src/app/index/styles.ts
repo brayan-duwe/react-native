b222e6ebe12c63f8692dfff366bfed75492296c7
Brayan Duwe <91811797+brayan-duwe@users.noreply.github.com>
2026-04-26 17:01:50 -0300

    index header

--- src/app/index/styles.ts
+++ src/app/index/styles.ts
@@ -4,11 +4,21 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    paddingTop: 62,
   },
   title: {
     color: colors.green[900],
     fontSize: 22,
+  },
+  header: {
+    paddingHorizontal: 24,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginBottom: 32,
+  },
+  logo: {
+    height: 32,
+    width: 38,
   },
 });
