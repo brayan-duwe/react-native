b11a220fff23c703b3dc39f7db93aaa4f29b207a
Brayan Duwe <91811797+brayan-duwe@users.noreply.github.com>
2026-05-08 21:08:09 -0300

    modal

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,5 +1,12 @@
 import { MaterialIcons } from "@expo/vector-icons";
-import { FlatList, Image, TouchableOpacity, View } from "react-native";
+import {
+  FlatList,
+  Image,
+  Modal,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 import { Categories } from "@/components/categories";
 import { Link } from "@/components/link";
@@ -31,6 +38,24 @@ export default function Index() {
         contentContainerStyle={styles.linksContent}
         showsVerticalScrollIndicator={false}
       />
+      <Modal transparent visible={true}>
+        <View style={styles.modal}>
+          <View style={styles.modalContent}>
+            <View style={styles.modalHeader}>
+              <Text style={styles.modalCategory}>Curso</Text>
+              <TouchableOpacity>
+                <MaterialIcons
+                  name="close"
+                  size={20}
+                  color={colors.gray[400]}
+                />
+              </TouchableOpacity>
+            </View>
+            <Text style={styles.modalLinkName}>Rocketseat</Text>
+            <Text style={styles.modalURL}>https://rocketseat.com.br</Text>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 }
